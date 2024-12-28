a761a0797837bd82c484108934fb0605e4bc5fe9
Harisha Swaminathan <hswaminathan@harishas-mbp.mynetworksettings.com>
2024-12-28 18:45:53 -0500

    fix formats

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,13 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { format } from "date-fns";
+import numeral from "numeral";
 
-const ExpenseListItem = ({description, amount, createdAt, id, dispatch}) => {
+const ExpenseListItem = ({description, amount, createdAt, id}) => {
    return  (
         <>
             <Link to={`/edit/${id}`}>
              <h3>{description}</h3>
             </Link> <br/>
-            <p>{amount} - {createdAt}</p>
+            <p>
+                {numeral(amount/100).format('$0,0.00')} 
+                -
+                {format(new Date(createdAt), "MMMM do, yyyy")}
+            </p>
         </>
     );
 }
